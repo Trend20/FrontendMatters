f2e905236f0d38671409b038ebacf139877e2cfc
Trend20 <enockomondi305@gmail.com>
2023-03-16 20:21:32 +0300

    styling the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
 
@@ -19,20 +19,81 @@ const Hero = () => {
     setEmail("");
   };
   return (
-    <div className="hero">
-      <h1>Frontend Matters</h1>
-      <p>Explore some of the most trending topics in the Frontend world!</p>
-      <form onSubmit={handleEmailSubscription}>
-        <input
+    <HeroContainer className="hero">
+      <HeroHeading>Frontend Matters</HeroHeading>
+      <HeroDescription>
+        Explore some of the most trending topics in the Frontend world! There
+        are different code snippets for every article explaining about a code
+        concept.
+      </HeroDescription>
+      <HeroForm onSubmit={handleEmailSubscription}>
+        <HeroFormInput
           type="email"
           placeholder="Your email address"
           value={email}
+          required
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input type="submit" value="Subscribe" />
-      </form>
-    </div>
+        <HeroFormButton type="submit">Subscribe</HeroFormButton>
+      </HeroForm>
+    </HeroContainer>
   );
 };
+
+const HeroContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 150px 0px;
+`;
+
+const HeroHeading = styled.h1`
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 2;
+`;
+
+const HeroDescription = styled.p`
+  font-size: 0.9rem;
+  font-weight: 400;
+  line-height: 2;
+  margin-top: 30px;
+  width: 50%;
+  text-align: center;
+`;
+
+const HeroForm = styled.form`
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+  width: 50%;
+`;
+
+const HeroFormInput = styled.input`
+  padding: 15px;
+  outline: none;
+  border-radius: 10px;
+  border: 0.5px solid grey;
+  width: 60%;
+
+  :focus {
+    border: 1px solid #2596be;
+    box-shadow: 0 0 0 0.1rem #2596be !important;
+  }
+`;
+
+const HeroFormButton = styled.button`
+  padding: 15px;
+  outline: none;
+  border-radius: 10px;
+  border: none;
+  width: 30%;
+  margin-left: 30px;
+  cursor: pointer;
+  background: #2596be;
+  font-weight: 500;
+  color: #fff;
+`;
 
 export default Hero;
