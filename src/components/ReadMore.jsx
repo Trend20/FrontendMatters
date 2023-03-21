b5636955f51e162f7332a5b8b50aa0ae9f4e187d
Trend20 <enockomondi305@gmail.com>
2023-03-21 17:05:33 +0300

    readmore button style

--- src/components/ReadMore.jsx
+++ src/components/ReadMore.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 const ReadMore = () => {
   return (
-    <div>
-      <button>Read More</button>
-    </div>
+    <ReadMoreContainer className="read-more">
+      <Button>Read More</Button>
+    </ReadMoreContainer>
   );
 };
 
 export default ReadMore;
+
+// styles
+const ReadMoreContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+`;
+
+const Button = styled.button`
+  display: flex;
+  padding: 15px;
+  border: none;
+  outline: none;
+  width: 10%;
+  justify-content: center;
+  align-items: center;
+  background: #2596be;
+  color: #fff;
+  cursor: pointer;
+`;
