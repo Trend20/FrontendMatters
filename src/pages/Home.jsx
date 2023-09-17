3dd7c73ccdc90645ea88dd9ccdd90806d1ad7d96
Trend20 <enockomondi305@gmail.com>
2023-09-17 15:53:06 +0300

    fetching articles from the supabase db

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,15 @@ import RecentPosts from "../components/RecentPosts";
 import PopularPosts from "../components/PopularPosts";
 import ReadMore from "../components/ReadMore";
 import Footer from "../components/Footer";
+import Posts from "./Posts";
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <RecentPosts />
-      <PopularPosts />
+        <Posts />
+      {/*<RecentPosts />*/}
+      {/*<PopularPosts />*/}
       <ReadMore />
       <Footer />
     </div>
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { posts } from "../data/posts";
 import Post from "../components/Post";
+import {useSelector} from "react-redux";
+import {supabase} from "../lib/api";
 
 const Posts = () => {
+  const {articles} = useSelector((store) => store.article);
+  const [data, setData] = useState([]);
+
+  // fetch the articles and populate on array
+  useEffect(() =>{
+   const getArticles = async () =>{
+     try {
+        const {data: articles, error} = await supabase.from('articles').select('*').order('id',{ ascending: false })
+       if (error) console.log("error", error);
+       else setData(articles);
+       console.log(data)
+     }catch (error){
+       console.log(error);
+     }
+   };
+   getArticles();
+  },[])
   return (
     <div className="posts">
       {posts.map((post, index) => {
