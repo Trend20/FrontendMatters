247a3e844c9b8f4b07d2fc5a5ebaf32406d3300a
Trend20 <enockomondi305@gmail.com>
2023-09-17 14:16:52 +0300

    adding add post component styles and state

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -9,14 +9,29 @@ const AddPost = () =>{
     const handleInputChange = (field, value) =>{
         dispatch(setInputValue({field, value}));
     }
+
+    // handle submit functionality
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+    }
     return(
-        <div>
-            <h1>Add Post Component</h1>
-            <form>
-                <input type="text" value={title} onChange={(e) => handleInputChange('title', e.target.value)}/>
-                <textarea name="description" value={description} onChange={(e) => handleInputChange('description', e.target.value)} id="description" cols="30" rows="10"></textarea>
-                <input type="text" value={author} onChange={(e) => handleInputChange('author', e.target.value)} />
-                <input type="file" value={coverImg} onChange={(e) => handleInputChange('coverImg', e.target.value)}/>
+        <div className='add-new-post'>
+            <h1>Add New Post</h1>
+            <form id='newPostForm'>
+                <div className="input">
+                    <input type="text" id='title' placeholder='Title' value={title} onChange={(e) => handleInputChange('title', e.target.value)}/>
+                </div>
+                <div className="input">
+                    <textarea name="description" placeholder='Description' value={description} onChange={(e) => handleInputChange('description', e.target.value)} id="description" cols="30" rows="10"></textarea>
+                </div>
+               <div className="input">
+                   <input type="text" id='author' placeholder='Author' value={author} onChange={(e) => handleInputChange('author', e.target.value)} />
+               </div>
+                <div className="input">
+                    <input type="file" id='img' value={coverImg} onChange={(e) => handleInputChange('coverImg', e.target.value)}/>
+                </div>
+                <button id='add-btn' type='submit'>Submit</button>
             </form>
         </div>
     )
