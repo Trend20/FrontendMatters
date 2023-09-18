af6067f95a0e565004dcef08cbb82c68a689a680
Trend20 <enockomondi305@gmail.com>
2023-09-18 16:46:30 +0300

    fetching articles and passing as props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Hero from "../components/Hero";
 import ReadMore from "../components/ReadMore";
 import Footer from "../components/Footer";
 import Posts from "./Posts";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import Empty from "../components/Empty";
+import {supabase} from "../lib/api";
+import {populateArticles} from "../features/slices/articleSlices";
 
 const Home = () => {
     const {articles} = useSelector((store) => store.article);
+    const [data, setData] = useState([]);
+    const dispatch = useDispatch();
+
+    // fetch the articles and populate on array
+    useEffect(() =>{
+        const getArticles = async () =>{
+            try {
+                const {data: articles, error} = await supabase.from('articles').select('*').order('id',{ ascending: false })
+                if (error) console.log("error", error);
+                else setData([...data, articles]);
+                dispatch(populateArticles(articles));
+                console.log(data)
+            }catch (error){
+                console.log(error);
+            }
+        };
+        getArticles();
+    },[])
   return (
     <div>
       <Hero />
         {
-            articles.length === 0 ? <Empty /> : <Posts />
+            articles.length === 0 ? <Empty /> : <Posts articles={articles} />
         }
         {
             articles.length > 6 &&  <ReadMore />
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,30 +1,6 @@
-import React, {useEffect, useState} from "react";
-import { posts } from "../data/posts";
 import Post from "../components/Post";
-import {useDispatch, useSelector} from "react-redux";
-import {supabase} from "../lib/api";
-import {populateArticles} from "../features/slices/articleSlices";
 
-const Posts = () => {
-  const {articles} = useSelector((store) => store.article);
-  const [data, setData] = useState([]);
-  const dispatch = useDispatch();
-
-  // fetch the articles and populate on array
-  useEffect(() =>{
-   const getArticles = async () =>{
-     try {
-        const {data: articles, error} = await supabase.from('articles').select('*').order('id',{ ascending: false })
-       if (error) console.log("error", error);
-       else setData([...data, articles]);
-       dispatch(populateArticles(data));
-       console.log(data)
-     }catch (error){
-       console.log(error);
-     }
-   };
-   getArticles();
-  },[])
+const Posts = ({articles}) => {
   return (
     <div className="posts">
       {articles.map((post, index) => {
