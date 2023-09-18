9198df0d89a3453e9458322f5e0df0476b9a7c79
Trend20 <enockomondi305@gmail.com>
2023-09-18 16:35:19 +0300

    styling empty component

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -1,6 +1,8 @@
+import emptyIcon from '../assets/img/empty.png'
 const Empty = () =>{
     return(
         <div className='empty-page'>
+            <img src={emptyIcon} alt="empty-icon" />
             <h1>No Article Available</h1>
         </div>
     )
