10dc32a7602605d78eafc0b2fae5e86a17c04447
Trend20 <enockomondi305@gmail.com>
2023-10-19 13:53:28 +0300

    loader and data fetching fixes

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../App.css";
+
+const Loader = () => {
+  return <div id="spinner"></div>;
+};
+
+export default Loader;
--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { supabase } from "../lib/api";
+import Loader from "./Loader";
 
 const SinglePost = () => {
   const [article, setArticle] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
   useEffect(() => {
@@ -15,27 +17,36 @@ const SinglePost = () => {
           .eq("id", articleId)
           .single();
         if (error) console.log("error", error);
-        else setArticle(data);
+        else setArticle({ ...article, data });
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
     getArticleById(id);
   }, [id]);
-  console.log(article);
+  console.log("article here", article);
+  const { title, description, timestamp, author } = article.data;
   return (
     <div className="single-post">
-      <div className="post-de">
-        <div className="post-title">
-          <h1>{article.title}</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="post-details">
+          <div className="post-title">
+            <h1>{title}</h1>
+          </div>
+          <div className="post-description">
+            <p>{description}</p>
+          </div>
+          <div className="post-author">
+            <p>{author}</p>
+          </div>
+          <div className="post-timestamp">
+            <p>{timestamp}</p>
+          </div>
         </div>
-        <div className="post-description">
-          <p>{article.description}</p>
-        </div>
-        <div className="post-timestamp">
-          <p>{article.timestamp}</p>m
-        </div>
-      </div>
+      )}
     </div>
   );
 };
